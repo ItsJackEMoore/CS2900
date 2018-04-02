6cca24cdc6b332fbcf0f097a84942adeb78a21dc
ItsJackEMoore <jmoore2@wpi.edu>
2018-04-02 00:25:40 -0400

    Moving ball
    Easier moving using PS.Line

---
+++ New/game04.js
@@ -0,0 +1,141 @@
+// game.js for Perlenspiel 3.2
+
+// The "use strict" directive in the following line is important. Don't alter or remove it!
+"use strict";
+
+// The following comment lines are for JSLint/JSHint. Don't alter or remove them!
+
+/*jslint nomen: true, white: true */
+/*global PS */
+
+/*
+This is a template for creating new Perlenspiel games.
+All event-handling functions are commented out by default.
+Uncomment and add code to the event handlers required by your project.
+*/
+
+/*
+PS.init( system, options )
+Called once after engine is initialized but before event-polling begins.
+[system] = an object containing engine and platform information; see API documentation for details.
+[options] = an object with optional parameters; see API documentation for details.
+*/
+
+// Uncomment the following BLOCK to expose PS.init() event handler:
+var ball = {
+    image: null,
+    x:5,
+    y:7,
+    touched: false
+};
+
+var level ={
+    level1: true,
+    level2: false,
+    timeL: 90
+
+
+};
+
+function redraw(x1,y1,x2,y2){
+    PS.pathmap(ball.image,x1,y1,x2,y2);
+}
+
+PS.init = function( system, options ) {
+	// Uncomment the following code line to verify operation:
+    PS.debug();
+	 PS.gridSize( 11, 11);
+	 PS.gridColor( 0x4468a3 );
+	 PS.borderColor(PS.ALL,PS.ALL,PS.COLOR_BLUE);
+
+	 PS.statusText( "Ball" );
+
+	 ball.image = PS.spriteSolid(1,1);
+	 PS.spriteMove(ball.image,ball.x,ball.y);
+
+
+    PS.audioLoad( "fx_coin4", { lock: true } ); // load & lock click sound
+    PS.audioLoad( "fx_coin5", { lock: true } );
+    PS.audioLoad( "fx_coin6", { lock: true } );
+    PS.audioLoad( "fx_coin7", { lock: true } );
+    PS.audioLoad( "fx_coin8", { lock: true } );
+
+
+    // Add any other initialization code you need here.
+};
+function playSound(){
+    var coin = Math.floor(Math.random() * 5) + 1
+
+    if(coin == 1 )
+    {
+        PS.audioPlay("fx_coin4");
+    }
+    else if(coin == 2)
+    {
+        PS.audioPlay("fx_coin5");
+    }
+
+    else if(coin == 3)
+    {
+        PS.audioPlay("fx_coin6");
+    }
+    else if(coin == 4)
+    {
+        PS.audioPlay("fx_coin7");
+    }
+    else if(coin == 5)
+    {
+        PS.audioPlay("fx_coin8");
+    }
+}
+function changeMomentum(x,y){
+    if(y < 7 && y > 7){
+
+    }
+    else if(y == 8){
+        ball.myM = 1;
+
+    }
+    else if(y >= 9){
+        ball.myM = 2;
+    }
+
+}
+
+PS.touch = function(x ,y,data,options){
+
+    if(x == ball.x && y == ball.y){
+        PS.statusText("Touched Ball");
+        ball.touched = true;
+    }
+
+};
+
+PS.release = function(x,y,data,opyions){
+    if(ball.touched == true)
+        var launchX = ball.x + (ball.x - x);
+        var launchY = ball.y + (ball.y - y);
+        var path = PS.line(ball.x,ball.y,launchX,launchY);
+        movement(path);
+
+
+
+
+
+
+};
+
+function movement(path){
+    var i;
+    for(i = 0; i < path.length; i++){
+        PS.statusText("worked");
+        var array = path[i];
+        var x = array[0];
+        var y = array[1];
+        PS.statusText(x + " " + y);
+        PS.spriteMove(ball.image,x,y);
+
+    }
+
+
+}
